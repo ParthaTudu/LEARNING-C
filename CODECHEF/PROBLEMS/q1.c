3423f2549f34652fc1998ee4a62ae38042e548ee
ParthaTudu <nepolio7partha@gmail.com>
2024-03-18 23:04:17 +0530

    ..

---
+++ CODECHEF/PROBLEMS/q1.c
@@ -0,0 +1,26 @@
+// Input Format
+// The only line of input contains two space separated integers 
+// 
+// X and 
+// 
+// Y — the threshold limit and the rate at which Chef is currently working at.
+
+// Output Format
+// If Chef is prone to errors print YES, otherwise print NO.
+
+// You may print each character of the string in uppercase or lowercase (for example, the strings yes, Yes, yEs, and YES will all be treated as identical).
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    // Your code goes here (if needed)
+       if(x < y ){
+        printf("YES");
+    }
+    else{
+      printf("NO");
+    }
+    return 0;
+}
